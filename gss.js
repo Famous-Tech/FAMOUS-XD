c90828fea40872e0f0d4e961df0e1100f49f42af
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2023-10-13 11:14:32 +0530

    update gss.js

--- gss.js
+++ gss.js
@@ -1461,7 +1461,7 @@ case '*download_app*':
 
         // Send the APK file as a document using sendMessage
         await gss.sendMessage(m.chat, { document: fs.readFileSync(outputPath),
-     mimetype: 'application/vnd.android.package-archive', filename: 'downloaded_app.apk', caption: 'downloaded by gss botwa' }, { quoted: m });
+ mimetype: 'application/vnd.android.package-archive', filename: 'downloaded_app.apk', caption: '' }, { quoted: m });
 
         // Optionally, you can delete the temporary file
         await fs.promises.unlink(outputPath);
