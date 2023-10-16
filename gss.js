8bac8e1d2da7cacb518906caf8f241f250cdf288
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2023-10-16 10:40:12 +0530

    update gss.js

--- gss.js
+++ gss.js
@@ -1702,7 +1702,19 @@ case 'gimage': {
 }
 break;
 
+case 'cry': case 'kill': case 'hug': case 'pat': case 'lick': 
+case 'kiss': case 'bite': case 'yeet': case 'bully': case 'bonk':
+case 'wink': case 'poke': case 'nom': case 'slap': case 'smile': 
+case 'wave': case 'awoo': case 'blush': case 'smug': case 'glomp': 
+case 'happy': case 'dance': case 'cringe': case 'cuddle': case 'highfive': 
+case 'shinobu': case 'handhold': {
 
+axios.get(`https://api.waifu.pics/sfw/${command}`)
+.then(({data}) => {
+gss.sendImageAsSticker(from, data.url, m, { packname: global.packname, author: 'gss botwa' })
+})
+}
+break
 
 
 case 'sticker': case 's': case 'stickergif': case 'sgif': {
