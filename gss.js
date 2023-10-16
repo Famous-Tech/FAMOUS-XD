6c69563fcf0524647253728b6667ea8ad9d4008d
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2023-10-16 12:25:19 +0530

    update gss.js

--- gss.js
+++ gss.js
@@ -1657,7 +1657,52 @@ case 'mediafire': {
     break;
 }
 
+case 'fb': case 'fbdl': case 'facebook': {
+    if (!args[0]) {
+        throw ` Please send the link of a Facebook video\n\nEXAMPLE :\n*${prefix + command} * https://fb.watch/7B5KBCgdO3`;
+    }
 
+    const urlRegex = /^(?:https?:\/\/)?(?:www\.)?(?:facebook\.com|fb\.watch)\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/i;
+    if (!urlRegex.test(args[0])) {
+        throw '⚠️ PLEASE GIVE A VALID URL.';
+    }
+
+    try {
+        const result = await fg.fbdl(args[0]);
+        const tex = `
+  *Video Details* 
+📽️ *Title*: ${result.title}
+`;
+
+
+        const response = await fetch(result.videoUrl);
+        const arrayBuffer = await response.arrayBuffer();
+        const videoBuffer = Buffer.from(arrayBuffer);
+
+        // Save the videoBuffer to a temporary file
+        const randomName = `temp_${Math.floor(Math.random() * 10000)}.mp4`;
+        fs.writeFileSync(`./${randomName}`, videoBuffer);
+
+        // Send the video using client.sendMessage
+        await gss.sendMessage(
+            m.chat,
+            {
+                video: fs.readFileSync(`./${randomName}`),
+                caption: tex,
+            },
+            { quoted: m }
+        );
+
+        // Delete the temporary file
+        fs.unlinkSync(`./${randomName}`);
+    } catch (error) {
+        console.log(error);
+        m.reply('⚠️ An error occurred while processing the request. Please try again later.');
+    }
+
+    break;
+}
+  
 
 case 'git': case 'gitclone':
   if (!args[0]) return m.reply(`Where is the link?\nExample :\n${prefix}${command} https://github.com/sid238/Gss_Botwa`)
