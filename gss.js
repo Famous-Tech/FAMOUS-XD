45fdae5e9dc3587f30cddc41f9eb2e150263de58
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2023-11-10 10:14:58 +0530

    update gss.js

--- gss.js
+++ gss.js
@@ -2014,51 +2014,30 @@ case 'dalle': case 'img': case 'image': {
 }
 break;
 
-case 'toanime': {
-  let q = m.quoted ? m.quoted : m
-  let mime = (q.msg || q).mimetype || q.mediaType || ""
-
-  if (!/image/g.test(mime)) throw '*Respond to an image*'
-
-  m.reply('*This command can turn your photo into anime*')
-
-  let data;
-if (q.download) {
-  try {
-    data = await q.download();
-  } catch (downloadError) {
-    throw `Error downloading the image: ${downloadError.message}`;
-  }
-} else {
-  throw 'No image to process.';
-}
-
-if (!data || !Buffer.isBuffer(data)) {
-  throw 'Invalid image data.';
-}
-
-let image = await uploadImage(data);
-
-
-  try {
-    let anime = `https://api.lolhuman.xyz/api/imagetoanime?apikey=${lolkeysapi}&img=${image}`
-    await gss.sendFile(m.chat, anime, 'error.jpg', null, m)
-  } catch (i) {
-    try {
-      let anime2 = `https://api.zahwazein.xyz/photoeditor/jadianime?url=${image}&apikey=${keysxxx}`
-      await gss.sendFile(m.chat, anime2, 'error.jpg', null, m)
-    } catch (a) {
-      try {
-        let anime3 = `https://api.caliph.biz.id/api/animeai?img=${image}&apikey=caliphkey`
-        await gss.sendFile(m.chat, anime3, 'error.jpg', null, m)
-      } catch (e) {
-        throw '*Error: Check if the person\'s face is visible*'
-      }
-    }
-  }
-}
-break;
-
+case "toanime":
+                let q = m.quoted ? m.quoted : m;
+                let mime = (q.msg || q).mimetype || q.mediaType || "";
+                if (!/image/g.test(mime)) throw "*Respond to a image*";
+                m.reply("*This command can turn your photo into anime*");
+                let data = await quoted.download();
+                let image = await uploadImage(data);
+                try {
+                    let anime = `https://api.lolhuman.xyz/api/imagetoanime?apikey=GataDios&img=${image}`;
+                    await m.reply(anime, { mimetype: "image/png" });
+                } catch (i) {
+                    try {
+                        let anime2 = `https://api.zahwazein.xyz/photoeditor/jadianime?url=${image}&apikey=6fb0eff124`;
+                        await m.reply(anime2, { mimetype: "image/png" });
+                    } catch (a) {
+                        try {
+                            let anime3 = `https://api.caliph.biz.id/api/animeai?img=${image}&apikey=caliphkey`;
+                            await m.reply(anime3, { mimetype: "image/png" });
+                        } catch (e) {
+                            throw "*Error check if the persons face is visible*";
+                        }
+                    }
+                }
+                break;
 
   
 case 'bug':
