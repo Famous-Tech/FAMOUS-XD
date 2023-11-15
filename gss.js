dd678af90207a28606539e036eb36c170bf78a50
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2023-11-15 08:27:41 +0530

    update gss.js

--- gss.js
+++ gss.js
@@ -1625,7 +1625,7 @@ case 'truecaller':
   try {
     if (!text) {
       // Reply when no phone number is provided
-      await gss.sendMessage(m.chat, 'Please provide a phone number.');
+      throw ('Please provide a phone number.');
       break;
     }
 
