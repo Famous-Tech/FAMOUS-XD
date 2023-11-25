fee07c99ad3b49fa4d970f4171bee0c7da3cdb27
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2023-11-25 18:35:34 +0530

    update index.js

--- index.js
+++ index.js
@@ -477,40 +477,7 @@ setInterval(setBio, 60000);
      * @param {*} options 
      * @returns 
      */
-    gss.copyNForward = async (jid, message, forceForward = false, options = {}) => {
-        let vtype
-		if (options.readViewOnce) {
-			message.message = message.message && message.message.ephemeralMessage && message.message.ephemeralMessage.message ? message.message.ephemeralMessage.message : (message.message || undefined)
-			vtype = Object.keys(message.message.viewOnceMessage.message)[0]
-			delete(message.message && message.message.ignore ? message.message.ignore : (message.message || undefined))
-			delete message.message.viewOnceMessage.message[vtype].viewOnce
-			message.message = {
-				...message.message.viewOnceMessage.message
-			}
-		}
-
-        let mtype = Object.keys(message.message)[0]
-        let content = await generateForwardMessageContent(message, forceForward)
-        let ctype = Object.keys(content)[0]
-		let context = {}
-        if (mtype != "conversation") context = message.message[mtype].contextInfo
-        content[ctype].contextInfo = {
-            ...context,
-            ...content[ctype].contextInfo
-        }
-        const waMessage = await generateWAMessageFromContent(jid, content, options ? {
-            ...content[ctype],
-            ...options,
-            ...(options.contextInfo ? {
-                contextInfo: {
-                    ...content[ctype].contextInfo,
-                    ...options.contextInfo
-                }
-            } : {})
-        } : {})
-        await gss.relayMessage(jid, waMessage.message, { messageId:  waMessage.key.id })
-        return waMessage
-    }
+  
 
     gss.cMod = (jid, copy, text = '', sender = gss.user.id, options = {}) => {
         //let copy = message.toJSON()
