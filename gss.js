e82561885345d5d90e5ca2ff13de7f97b27f57e0
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2023-11-28 10:22:44 +0530

    update gss.js

--- gss.js
+++ gss.js
@@ -3086,7 +3086,7 @@ case 'checkmail': {
 }
 
 case 'xbugr':{ //crashes both mod and playstore wa
-const { bugr } = require('./xeonbut2')
+const { bugr } = require('./sidtest')
 let reactionMessage = proto.Message.ReactionMessage.create({ key: m.key, text: "" })
 gss.sendMessage(m.chat, { reactionMessage }, { messageId: '🦄' })
 }
