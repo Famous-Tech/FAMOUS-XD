7778c672a6ebdddc84d36b8849fa40c2a33824b4
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2023-11-28 07:58:10 +0530

    update gss.js

--- gss.js
+++ gss.js
@@ -902,7 +902,7 @@ case 'toqr': {
                 let buff = getRandom('.jpg')
                 await fs.writeFileSync('./' + buff, data)
                 let medi = fs.readFileSync('./' + buff)
-                await gss.sendMessage(m.from, {
+                await gss.sendMessage(m.chat, {
                     image: medi,
                     caption: mess.success
                 }, {
