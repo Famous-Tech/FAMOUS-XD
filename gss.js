455d854f482f5f87dcabb229d0788e6987c4d397
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2023-12-01 17:15:36 +0530

    update gss.js

--- gss.js
+++ gss.js
@@ -1712,32 +1712,47 @@ for (let i = 0; i < data.data.length; i++) {
 break;
 
 
-case 'play2':{
+case 'play2': {
   if (!text) return m.reply('Enter YouTube Video Link or Search Query!');
- 
-  const apiURL = `https://ytsearch-4rtb.onrender.com/api?search=${encodeURIComponent(text)}`;
-  const topResult = data.data[0];
-  const { title, url } = topResult;
-  
-  gss.sendPoll(m.chat, `Tujhe ka need he ?\n${title}`,
-    [`${command.charAt(0).toUpperCase() + command.slice(1)} Audio ${url}`,
-    `${command.charAt(0).toUpperCase() + command.slice(1)} Video ${url}`]);
- 
-  if (args[0].toLowerCase() === 'audio') {
-    //Add Logic to Download Auduo
-    const yturl = args[1]
-    m.reply(`Ye URL ko tera Auduo Downloader API me pass kar ${yturl}`)
 
-  } else if (args[0].toLowerCase() === 'video') {
-    //Add Logic To Download Video
-    const yturl = args[1]
-    m.reply(`Ye URL ko tera Video Downloader API me pass kar ${yturl}`)
+  try {
+    const apiURL = `https://ytsearch-4rtb.onrender.com/api?search=${encodeURIComponent(text)}`;
+    const response = await fetch(apiURL);
+    const data = await response.json();
 
-  } else {
-    
+    if (data.type === 'search' && Array.isArray(data.data) && data.data.length > 0) {
+      const topResult = data.data[0];
+      const { title, url } = topResult;
+
+      gss.sendPoll(m.chat, `Tujhe ka need he?\n${title}`, [
+        `Play Audio ${url}`,
+        `Play Video ${url}`
+      ]);
+
+      if (args[0].toLowerCase() === 'audio') {
+        // Add Logic to Download Audio
+        const yturl = args[1];
+        m.reply(`Ye URL ko tera Audio Downloader API me pass kar ${yturl}`);
+
+      } else if (args[0].toLowerCase() === 'video') {
+        // Add Logic To Download Video
+        const yturl = args[1];
+        m.reply(`Ye URL ko tera Video Downloader API me pass kar ${yturl}`);
+
+      } else {
+        // Additional logic if needed
+      }
+    } else {
+      console.error('Invalid API response:', data);
+      return m.reply('Error retrieving search results.');
+    }
+  } catch (error) {
+    console.error('Error during yts:', error);
+    return m.reply('Unexpected error occurred.');
   }
 }
 break;
+
 
 case 'play': {
   if (!text) {
