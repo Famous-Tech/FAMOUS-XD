f397386f7db9e17c8adc001264455990695ad49d
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2023-12-12 16:50:14 +0530

    update gss.js

--- gss.js
+++ gss.js
@@ -2990,73 +2990,32 @@ break;
   break;
   
 
-const convertToPdf = async (media, isVideo, text) => {
-  const pdfDoc = await PDFDocument.create();
-  const page = pdfDoc.addPage();
-  const { width, height } = page.getSize();
-
-  if (isVideo) {
-    // Convert video to image
-    const imageBuffer = await promisify(ffmpeg().input(media).outputFormat('image2').outputOptions('-vframes 1').toBuffer)();
-    const image = await pdfDoc.embedPng(imageBuffer);
-    page.drawImage(image, {
-      x: 0,
-      y: 0,
-      width: width,
-      height: height,
-    });
-  } else if (text) {
-    // Add text to PDF
-    const font = await pdfDoc.embedFont(PDFDocument.Font.Helvetica);
-    const textSize = 12;
-    page.drawText(text, { x: 50, y: height - 50, font, size: textSize });
-  } else {
-    // Convert image to PDF
-    const image = await pdfDoc.embedPng(media);
-    page.drawImage(image, {
-      x: 0,
-      y: 0,
-      width: width,
-      height: height,
-    });
-  }
-
-  // Save PDF to buffer
-  const pdfBytes = await pdfDoc.save();
-  return pdfBytes;
-};
-
-
 case 'pdf': {
-  let pdfContent = '';
-
-  // Check if it's an image, video, or text
   if (/image|video/.test(mime)) {
     m.reply(mess.wait);
     let media = await gss.downloadMediaMessage(qmsg);
-
+    
+    // Check if it's an image
+    if (/image/.test(mime)) {
+      await gss.sendImageAsSticker(m.chat, media, m, { packname: global.packname, author: global.author });
+    }
+    
     // Check if it's a video
-    const isVideo = /video/.test(mime);
+    else if (/video/.test(mime)) {
+      if (qmsg.seconds > 11) return m.reply('Maximum duration is 10 seconds!');
+      await gss.sendVideoAsSticker(m.chat, media, m, { packname: global.packname, author: global.author });
+    }
 
     // Convert media to PDF
-    const pdfBytes = await convertToPdf(media, isVideo, pdfContent);
-
+    let pdfBuffer = /* your code to convert media to PDF */;
+    
     // Send the PDF file
-    await gss.sendMessage(m.chat, pdfBytes, 'media.pdf', 'Here is your media in PDF format!', null, { mimetype: 'application/pdf' });
+    await m.sendFile(m.chat, pdfBuffer, 'media.pdf', `Here is your media in PDF format!`, null, { mimetype: 'application/pdf' });
 
     // Remove temporary files
     await fs.unlinkSync(media);
-  } else if (/text/.test(mime)) {
-    // Extract text from the message
-    pdfContent = m.text.substr(5); // Assuming the command is "!pdf "
-    
-    // Convert text to PDF
-    const pdfBytes = await convertToPdf(null, false, pdfContent);
-
-    // Send the PDF file
-    await gss.sendMessage(m.chat, pdfBytes, 'text.pdf', 'Here is your text in PDF format!', null, { mimetype: 'application/pdf' });
   } else {
-    m.reply(`Send/reply with an image, video, or text with caption ${prefix + command}`);
+    m.reply(`Send/reply with an image/video/gif with caption ${prefix + command}\nVideo/Gif duration 1-9 seconds`);
   }
 }
 break;
