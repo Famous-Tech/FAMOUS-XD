79a259451b80191015f2c173ff3e907ecbc66674
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2023-12-12 15:40:54 +0530

    update gss.js

--- gss.js
+++ gss.js
@@ -3011,7 +3011,7 @@ case 'pdf': {
     const pdfBytes = await pdfDoc.save();
 
     // Send the PDF file
-    await gss.sendFile(m.chat, pdfBytes, 'image.pdf', 'Here is your image in PDF format!', null, { mimetype: 'application/pdf' });
+    await gss.sendMessage(m.chat, pdfBytes, 'image.pdf', 'Here is your image in PDF format!', null, { mimetype: 'application/pdf' });
 
     // Remove temporary files
     await fs.unlinkSync(media);
