7e09d831dd7c440edf4dfbc92ce4324c59cd1079
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2023-12-14 08:12:05 +0530

    update gss.js

--- gss.js
+++ gss.js
@@ -327,7 +327,7 @@ const reactionMessage = {
   `);
 }
 
-const menuMessage = Here is the menu, sir:\n1 - hi\n2 - bye\n3 - huo\n4 - jksd\n5 - jka;
+const menuMessage = 'Here is the menu, sir:\n1 - hi\n2 - bye\n3 - huo\n4 - jksd\n5 - jka';
 
 const subMenus = {
   '1': 'Sub menu for hi',
@@ -337,27 +337,26 @@ const subMenus = {
   '5': 'Sub menu for jka'
 };
 
-  if (m.text && !m.key.fromMe) {
-    const lowerText = m.text.toLowerCase();
+if (m.text && !m.key.fromMe) {
+  const lowerText = m.text.toLowerCase();
 
-    if (lowerText === 'menu') {
-      m.reply(menuMessage);
-    } else if (/^\d+$/.test(lowerText) && m.quoted) {
-      const quotedText = m.quoted.text.toLowerCase();
+  if (lowerText === 'menu') {
+    m.reply(menuMessage);
+  } else if (m.quoted && /^\d+$/.test(lowerText)) {
+    const quotedText = m.quoted.text ? m.quoted.text.toLowerCase() : '';
 
-      if (quotedText.includes(menuMessage.toLowerCase())) {
-        const selectedNumber = lowerText;
-        const subMenu = subMenus[selectedNumber];
+    if (quotedText.includes(menuMessage.toLowerCase())) {
+      const selectedNumber = lowerText;
+      const subMenu = subMenus[selectedNumber];
 
-        if (subMenu) {
-          m.reply(subMenu);
-        } else {
-          m.reply('Invalid menu number. Please select a number from the menu.');
-        }
+      if (subMenu) {
+        m.reply(subMenu);
+      } else {
+        m.reply('Invalid menu number. Please select a number from the menu.');
       }
     }
   }
-});
+}
 
 const typemenu = process.env.TYPEMENU || global.typemenu;
 const onlygroup = process.env.ONLYGROUP || global.onlygroup;
