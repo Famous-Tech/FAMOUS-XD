f3565ca8ae507cd7ded1ce57bad6a4864e1151f4
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2023-12-16 01:06:17 +0530

    update gss.js

--- gss.js
+++ gss.js
@@ -4435,7 +4435,6 @@ const apiUrl = 'https://vihangayt.me/download/fmmods';
                 }
             }
         }
-    }
 } catch (error) {
     console.error('Error fetching data from the API:', error.message);
     await m.reply('Error fetching data. Please try again later.');
