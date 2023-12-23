cdd50b565d894a472d9a0099ba870778d0b85e8a
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2023-12-23 20:18:58 +0530

    update gss.js

--- gss.js
+++ gss.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 require('./config')
 const Func = ('./lib/function.js');
 const fonts = require('./lib/font.js');
+const YT = require('./lib/YTModule.js');
 const more = String.fromCharCode(8206)
 const readmore = more.repeat(4001)
 const availableStyles = Object.keys(fonts);
@@ -1671,6 +1672,39 @@ fs.unlinkSync(`./${randomName}`);
     m.reply('Unexpected error occurred.');
     await doReact("❌");
   }
+  break;
+
+case 'yta':
+case 'song':
+case 'ytmp3':
+case 'audio':
+  if (!text) {
+    await doReact('❌');
+    return m.reply("Provide a song name");
+  }
+
+  const search = text.trim();
+  const { videos } = await yts(search);
+
+  if (!videos || videos.length <= 0)
+    return m.reply('Sorry, cannot be found');
+
+  const audio = new YT(videos[0].url, "audio");
+
+  if (!audio.url) return;
+
+  gss.sendMessage(m.from, await audio.getBuffer(), 2, {
+    quoted: m,
+    contextInfo: {
+      externalAdReply: {
+        title: videos[0].title.substr(0, 30),
+        mediaType: 2,
+        thumbnail: await fs.promises.readFile(`https://i.ytimg.com/vi/${audio.id}/hqdefault.jpg`),
+        mediaUrl: audio.url,
+      },
+    },
+  })
+    .catch(reason => m.reply(`An error occurred.`));
   break;
 
 
