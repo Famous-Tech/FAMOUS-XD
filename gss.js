779d9b1c4115c89f74465dd79c4e24266f80d0f0
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2023-12-24 18:40:38 +0530

    update gss.js

--- gss.js
+++ gss.js
@@ -345,6 +345,31 @@ const reactionMessage = {
 }
 
 
+if (m.text.toLowerCase() === 'send') {
+    try {
+        const quotedMessage = m.msg.contextInfo.quotedMessage;
+
+        if (quotedMessage) {
+            // Check if it's an image
+            if (quotedMessage.imageMessage) {
+                let imageCaption = quotedMessage.imageMessage.caption;
+                let imageUrl = await gss.downloadAndSaveMediaMessage(quotedMessage.imageMessage);
+                gss.sendMessage(m.chat, { image: { url: imageUrl }, caption: imageCaption });
+            }
+
+            // Check if it's a video
+            if (quotedMessage.videoMessage) {
+                let videoCaption = quotedMessage.videoMessage.caption;
+                let videoUrl = await gss.downloadAndSaveMediaMessage(quotedMessage.videoMessage);
+                gss.sendMessage(m.chat, { video: { url: videoUrl }, caption: videoCaption });
+            }
+        }
+    } catch (error) {
+        console.error("Error in 'send message' handling:", error);
+    }
+}
+
+
     
     const apiUrl = 'https://vihangayt.me/download/fmmods';
 
@@ -2814,33 +2839,8 @@ let link = 'https://chat.whatsapp.com/' + await gss.groupInviteCode(group)
 }
 break
 
-case 'take':
-case 'send':
-case 'save': {
-    if (!m.quoted) return m.reply('Please mention a message.');
-    try {
-        const quotedMessage = m.msg.contextInfo.quotedMessage;
 
-        if (quotedMessage) {
-            // Check if it's an image
-            if (quotedMessage.imageMessage) {
-                let imageCaption = quotedMessage.imageMessage.caption;
-                let imageUrl = await gss.downloadAndSaveMediaMessage(quotedMessage.imageMessage);
-                gss.sendMessage(m.chat, { image: { url: imageUrl }, caption: imageCaption });
-            }
-
-            // Check if it's a video
-            if (quotedMessage.videoMessage) {
-                let videoCaption = quotedMessage.videoMessage.caption;
-                let videoUrl = await gss.downloadAndSaveMediaMessage(quotedMessage.videoMessage);
-                gss.sendMessage(m.chat, { video: { url: videoUrl }, caption: videoCaption });
-            }
-        }
-    } catch (error) {
-        console.error("Error in 'send message' handling:", error);
-    }
-    break;
-}
+    
 
 
 case 'fb': case 'fbdl': case 'facebook': {
