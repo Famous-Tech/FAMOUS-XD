af432c3f22d10caf95f22d2705b46add780471ff
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2023-12-28 00:00:02 +0530

    update gss.js

--- gss.js
+++ gss.js
@@ -1569,6 +1569,32 @@ case 'whatmusic': case 'find': case 'shazame':
     break;
 
 
+case 'fetch1':
+    if (!/^https?:\/\//.test(text)) throw 'Awali *URL* dengan http:// atau https://';
+    const _url = new URL(text);
+    const url = global.API(_url.origin, _url.pathname, Object.fromEntries(_url.searchParams.entries()), 'APIKEY');
+    const res = await fetch(url);
+    
+    if (res.headers.get('content-length') > 100 * 1024 * 1024 * 1024) {
+        // delete res
+        throw `Content-Length: ${res.headers.get('content-length')}`;
+    }
+
+    if (!/text|json/.test(res.headers.get('content-type'))) {
+        return gss.sendFile(m.chat, url, 'file', text, m);
+    }
+
+    let txt = await res.buffer();
+
+    try {
+        txt = format(JSON.parse(txt + ''));
+    } catch (e) {
+        txt = txt + '';
+    } finally {
+        m.reply(txt.slice(0, 65536) + '');
+    }
+    break;
+
 
 
 case 'ebinary': {
