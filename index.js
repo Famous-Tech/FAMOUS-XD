768701c9489c1a6a04cd5296281336452713c642
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2024-01-03 13:58:11 +0530

    update index.js

--- index.js
+++ index.js
@@ -113,7 +113,9 @@ gss.ev.on("call", async (json) => {
         mek.message = (Object.keys(mek.message)[0] === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
         if (mek.key && mek.key.remoteJid === 'status@broadcast') return
         if (!gss.public && !mek.key.fromMe && chatUpdate.type === 'notify') return
-        if (mek.key.id.startsWith('BAE5') && mek.key.id.length === 16) return
+        if (mek.key.id.startsWith('BAE5') && mek.key.id.length === 16) {
+console.log('Bot Detected');
+}
         if (mek.key.id.startsWith('FatihArridho_')) return
         m = smsg(gss, mek, store)
         require("./gss")(gss, m, chatUpdate, store)
