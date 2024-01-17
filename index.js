363c71607d99522935e88de24f7a8f8e96d47f60
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2024-01-17 21:53:51 +0530

    update index.js

--- index.js
+++ index.js
@@ -150,10 +150,9 @@ gss.ev.on('messages.update', async chatUpdate => {
                 var prefCmd = prefix + toCmd;
 
                 try {
-                    // Delete the poll message after 20 seconds
                     setTimeout(async () => {
                         await gss.sendMessage(key.remoteJid, { delete: key });
-                    }, 20000);
+                    }, 10000);
                 } catch (error) {
                     console.error("Error deleting message:", error);
                 }
