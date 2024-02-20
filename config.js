61af9349856a8302971f454849efcd65dd31fd06
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2024-02-20 15:13:58 +0530

    update config.js

--- config.js
+++ config.js
@@ -32,14 +32,14 @@ global.prefa = ['', '!', '.'];
 global.sp = '✪';
 global.mess = {
     success: '```✓ Success```',
-    admin: '```This feature is only for group admins```',
-    botAdmin: '```I am not an admin!```',
-    owner: '```You are not my owner```',
-    group: '```You can use this command only in groups ❌```',
-    private: '```Feature is used only for private chats!```',
-    bot: '```Bot number user special features```',
+    admin: '`This feature is only for group admins`',
+    botAdmin: '`I am not an admin!`',
+    owner: '`You are not my owner`',
+    group: '`You can use this command only in groups ❌`',
+    private: '`Feature is used only for private chats!`',
+    bot: '`Bot number user special features`',
     wait: '*Processing Your request*',
-    endLimit: '```Your daily limit has expired, the limit will be reset every 12 hours```',
+    endLimit: '`Your daily limit has expired, the limit will be reset every 12 hours`',
 };
 global.link = 'https://whatsapp.com/channel/0029VaFNrVHBfxoBuY2TzH2w'
 global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
