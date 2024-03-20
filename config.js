b55706a954c7b04386038bbcc891c037bca19abb
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2024-03-20 15:07:02 +0500

    update config.js

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ global.APIs = {
 // Other
 global.apikey = 'bf2d2cf29b3edc604b447983';
 global.ownername = '𝑆𝛪𝐷 𝐵𝛨𝛥𝛪';
-global.owner = ['917050906659'];
+global.owner = ['917050906659', '917004950957'];
 global.premium = ['917050906659'];
 global.packname = '𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢';
 global.author = '𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫';
