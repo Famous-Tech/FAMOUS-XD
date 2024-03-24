7abb55c438153934cf4176cc983f95c2082490f5
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2024-03-24 13:48:28 +0500

    update gss.js

--- gss.js
+++ gss.js
@@ -56,7 +56,7 @@ const acr = new acrcloud({
     access_key: 'c33c767d683f78bd17d4bd4991955d81',
     access_secret: 'bvgaIAEtADBTbLwiPGYlxupWqkNGIjT7J9Ag2vIu'
 });
-const apiKey = "AIzaSyCA15iwOZZS-FCVIx3JmGklYFUOoS42zeE";
+const apiKey = "AIzaSyChpx8N6gNWPOZoKCsJxbdnVbNvolEoito";
 const genAI = new GoogleGenerativeAI(apiKey);
 const tempMailAddresses = {};
 const defaultLang = 'en'
