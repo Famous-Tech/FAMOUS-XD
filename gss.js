e1b21f40a6c59550cc194947f0c039b4a4119612
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2024-03-25 14:44:19 +0500

    update gss.js

--- gss.js
+++ gss.js
@@ -506,18 +506,14 @@ if (!('autobio' in setting)) setting.autobio = false
         
 
 /*antiviewonce*/
-    if ((db.data.chats[m.chat].antiviewonce || db.data.chats[m.chat].antiviewonce) && m.mtype == 'viewOnceMessageV2') {
-    let val = { ...m };
-    let msg = val.message?.viewOnceMessage?.message || val.message?.viewOnceMessageV2?.message;
-    if (m.isGroup) {
-        if (m.isBaileys && m.fromMe) return;
-        delete msg[Object.keys(msg)[0]].viewOnce;
-        val.message = msg;
-        await gss.sendMessage(m.chat, { forward: val }, { quoted: m });
-    } else {
-        await gss.sendMessage(m.chat, msg, { quoted: m });
+    if ( db.data.chats[m.chat].antiviewonce && m.isGroup && m.mtype == 'viewOnceMessageV2') {
+    	if (m.isBaileys && m.fromMe) return
+        let val = { ...m }
+        let msg = val.message?.viewOnceMessage?.message || val.message?.viewOnceMessageV2?.message
+        delete msg[Object.keys(msg)[0]].viewOnce
+        val.message = msg
+        await gss.sendMessage(m.chat, { forward: val }, { quoted: m })
     }
-}
 
 
 /*AUTOBIO*/
