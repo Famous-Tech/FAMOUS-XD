389e1f1a09a2b819cc5be475f9548e24a3c20bd1
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2024-03-30 13:27:41 +0500

    update gss.js

--- gss.js
+++ gss.js
@@ -1040,7 +1040,7 @@ try {
             // Handle download as audio
             if (videoUrl) {
                 const audioStream = ytdl(videoUrl, { filter: 'audioonly' });
-                await gss.sendMessage(m.chat, { audio: { url: videoStream } }, { quoted: m });
+                await gss.sendMessage(m.chat, { video: audioStream, mimetype: 'audio/mpeg', caption: captionMessage });
 
             } else {
                 await gss.sendMessage(m.chat, { text: 'No valid audio URL found in the quoted message.' }, { quoted: m });
@@ -1049,7 +1049,7 @@ try {
             // Handle download as video
             if (videoUrl) {
                 const videoStream = ytdl(videoUrl, { filter: 'audioandvideo', quality: 'highest' });
-                await gss.sendMessage(m.chat, { video: { url: videoStream } }, { quoted: m });
+                await gss.sendMessage(m.chat, { video: videoStream, mimetype: 'video/mp4', caption: captionMessage });
             } else {
                 await gss.sendMessage(m.chat, { text: 'No valid video URL found in the quoted message.' }, { quoted: m });
             }
