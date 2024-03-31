3c22b4028ae6cbda5389d9c4cb1dd9b514f66b95
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2024-03-31 15:09:23 +0500

    update config.js

--- config.js
+++ config.js
@@ -3,6 +3,9 @@ const chalk = require('chalk');
 
 
 // Other
+global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
+global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
+
 global.apikey = 'bf2d2cf29b3edc604b447983';
 global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
 global.owner = ['917050906659'];
@@ -10,17 +13,14 @@ global.premium = ['917050906659'];
 global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
 global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
 global.sessionName = 'session';
-global.prefa = ['!', '.'];
-global.sp = '✪';
-global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
-global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
+global.prefa = ["."];
+global.typemenu = "v1"
 global.autoswview = true
 global.autoread = false
 global.available = false
 global.autoTyping = false
 global.autoRecord = false
 global.autoreact = false
-global.typemenu = "v1"
 global.onlygroup = false
 global.onlypc = false
 global.welcome = true
