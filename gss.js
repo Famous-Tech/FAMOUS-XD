6dbdc496b08fe30f2058a3fd657731b144bc8036
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2024-04-02 11:37:25 +0500

    update gss.js

--- gss.js
+++ gss.js
@@ -899,21 +899,20 @@ const subMenus = {
 
 const lowerText = m.text.toLowerCase();
 
-if (command === 'menu') {
-    if (menuType === '1') {
-        await gss.sendMessage(m.chat, {
-            image: { url: 'https://telegra.ph/file/61eec5ebaeef2a046a914.jpg' },
-            caption: menuMessage,
-            contextInfo: {
-                externalAdReply: {
-                    showAdAttribution: false,
-                    title: botname,
-                    sourceUrl: global.link,
-                    body: `Bot Created By ${global.owner}`
-                }
+if (command === 'menu' && (menuType === '1')) {
+    await gss.sendMessage(m.chat, {
+        image: { url: 'https://telegra.ph/file/61eec5ebaeef2a046a914.jpg' },
+        caption: menuMessage,
+        contextInfo: {
+            externalAdReply: {
+                showAdAttribution: false,
+                title: botname,
+                sourceUrl: global.link,
+                body: `Bot Created By ${global.owner}`
             }
-        }, { quoted: m });
-    } else if (/^\d+$/.test(lowerText) && m.quoted) {
+        }
+    }, { quoted: m });
+} else if (/^\d+$/.test(lowerText) && m.quoted) {
         const quotedText = m.quoted.text.toLowerCase();
 
         if (quotedText.includes(menuMessage.toLowerCase())) {
@@ -933,15 +932,62 @@ if (command === 'menu') {
                 }
             }
         }, { quoted: m });
+            } else {
+                await gss.sendMessage(m.chat, {text: 'Invalid menu number. Please select a number from the menu.'}, { quoted: m });
+            }
         }
-    } else if (menuType === '2') {
-        if (isBan) return m.reply(mess.banned);
-        if (isBanChat) return m.reply(mess.bangc);
-        await gss.sendPoll(m.chat, "List Menu", ['.Allmenu', '.Groupmenu', '.Downloadmenu', '.Searchmenu', '.Funmenu', '.Toolmenu', '.Convertmenu', '.aimenu', '.Mainmenu', '.Ownermenu'], { quoted: m });
-    } else {
-        await gss.sendMessage(m.chat, { text: 'Invalid menu type. Please check the configuration.' }, { quoted: m });
+    }
+
+if (command === 'menu' && menuType === '1') {
+    await gss.sendMessage(m.chat, {
+        image: { url: 'https://telegra.ph/file/61eec5ebaeef2a046a914.jpg' },
+        caption: menuMessage,
+        contextInfo: {
+            externalAdReply: {
+                showAdAttribution: false,
+                title: botname,
+                sourceUrl: global.link,
+                body: `Bot Created By ${global.owner}`
+            }
+        }
+    }, { quoted: m });
+} else if (/^\d+$/.test(lowerText) && m.quoted) {
+    const quotedText = m.quoted.text.toLowerCase();
+
+    if (quotedText.includes(menuMessage.toLowerCase())) {
+        const selectedNumber = lowerText;
+        const subMenu = subMenus[selectedNumber];
+
+        if (subMenu !== undefined) {
+            await gss.sendMessage(m.chat, {
+                image: { url: 'https://telegra.ph/file/61eec5ebaeef2a046a914.jpg' },
+                caption: subMenu,
+                contextInfo: {
+                    externalAdReply: {
+                        showAdAttribution: false,
+                        title: botname,
+                        sourceUrl: global.link,
+                        body: `Bot Created By ${global.owner}`
+                    }
+                }
+            }, { quoted: m });
+        } else {
+            await gss.sendMessage(m.chat, { text: 'Invalid menu number. Please select a number from the menu.' }, { quoted: m });
+        }
     }
 }
+
+    
+if (command === 'menu' && menuType === '2') {
+    if (isBan) return m.reply(mess.banned);
+    if (isBanChat) return m.reply(mess.bangc);
+    
+    await gss.sendPoll(m.chat, "List Menu", ['.Allmenu', '.Groupmenu', '.Downloadmenu', '.Searchmenu', '.Funmenu', '.Toolmenu', '.Convertmenu', '.aimenu', '.Mainmenu', '.Ownermenu'], { quoted: m });
+} else {
+    await gss.sendMessage(m.chat, { text: 'Invalid menu type. Please check the configuration.' }, { quoted: m });
+}
+
+
 
 	    
         switch(command) {
