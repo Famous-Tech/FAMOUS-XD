68c4cb72d1b65fe04046e552d3dddb877cf52c40
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2024-04-02 06:28:08 +0500

    update gss.js

--- gss.js
+++ gss.js
@@ -901,30 +901,12 @@ const subMenus = {
 
 const lowerText = m.text.toLowerCase();
 
-if (command === 'menu2') {
-    await gss.sendMessage(m.chat, {
-        image: { url: 'https://telegra.ph/file/61eec5ebaeef2a046a914.jpg' },
-        caption: menuMessage,
-        contextInfo: {
-            externalAdReply: {
-                showAdAttribution: false,
-                title: botname,
-                sourceUrl: global.link,
-                body: `Bot Created By ${global.owner}`
-            }
-        }
-    }, { quoted: m });
-} else if (/^\d+$/.test(lowerText) && m.quoted) {
-        const quotedText = m.quoted.text.toLowerCase();
-
-        if (quotedText.includes(menuMessage.toLowerCase())) {
-            const selectedNumber = lowerText;
-            const subMenu = subMenus[selectedNumber];
-
-            if (subMenu !== undefined) {
-                await gss.sendMessage(m.chat, {
+const menuType = global.menuType; 
+if (command === 'menu') {
+    if (menuType === 1) {
+        await gss.sendMessage(m.chat, {
             image: { url: 'https://telegra.ph/file/61eec5ebaeef2a046a914.jpg' },
-            caption: subMenu,
+            caption: menuMessage,
             contextInfo: {
                 externalAdReply: {
                     showAdAttribution: false,
@@ -934,11 +916,15 @@ if (command === 'menu2') {
                 }
             }
         }, { quoted: m });
-            } else {
-                await gss.sendMessage(m.chat, {text: 'Invalid menu number. Please select a number from the menu.'}, { quoted: m });
-            }
-        }
+    } else if (menuType === 2) {
+        if (isBan) return m.reply(mess.banned);
+        if (isBanChat) return m.reply(mess.bangc);
+        gss.sendPoll(m.chat, "List Menu", ['.Allmenu', '.Groupmenu', '.Downloadmenu', '.Searchmenu', '.Funmenu', '.Toolmenu', '.Convertmenu', '.aimenu', '.Mainmenu', '.Ownermenu'], { quoted: m });
+    } else {
+        await gss.sendMessage(m.chat, {text: 'Invalid menu type. Please check the configuration.'}, { quoted: m });
     }
+}
+
 
 
 
