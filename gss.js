16f80171534d369801608c61bdb54940210d32be
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2024-04-08 23:19:20 +0500

    update gss.js

--- gss.js
+++ gss.js
@@ -906,7 +906,6 @@ const subMenus = {
 const lowerText = m.text.toLowerCase();
 
 if (command === 'menu') {
-    if (menuType === '1') {
         await gss.sendMessage(m.chat, {
             image: { url: 'https://telegra.ph/file/61eec5ebaeef2a046a914.jpg' },
             caption: menuMessage,
@@ -919,11 +918,6 @@ if (command === 'menu') {
                 }
             }
         }, { quoted: m });
-    } else if (menuType === '2') {
-        if (isBan) return m.reply(mess.banned);
-        if (isBanChat) return m.reply(mess.bangc);
-        
-        await gss.sendPoll(m.chat, "List Menu", ['.Allmenu', '.Groupmenu', '.Downloadmenu', '.Searchmenu', '.Funmenu', '.Toolmenu', '.Convertmenu', '.aimenu', '.Mainmenu', '.Ownermenu'], { quoted: m });
     } else if (/^\d+$/.test(lowerText) && m.quoted) {
         const quotedText = m.quoted.text.toLowerCase();
 
@@ -949,7 +943,6 @@ if (command === 'menu') {
             }
         }
     }
-}
 
 
 	    
