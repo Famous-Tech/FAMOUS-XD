e2357525b6e6544e89012c8dc8ce384fc6d9f2a0
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2024-04-28 17:54:40 +0530

    update gss.js

--- gss.js
+++ gss.js
@@ -3433,19 +3433,16 @@ async function downloadAndSendMedia(m, text, isDocument) {
             const mediaType = mediaUrl.endsWith('.mp4') ? 'video' : 'image';
             const fileName = `instagram_media.${mediaType === 'image' ? 'jpg' : 'mp4'}`;
 
-            if (isDocument) {
-                await gss.sendMessage({ document: fileBuffer, mimetype: `video/mp4`, filename: fileName }, { quoted: m });
-            } else {
-                if (mediaType === 'image') {
-                    await gss.sendMessage({ image: fileBuffer, mimetype: 'image/jpeg', filename: fileName }, { quoted: m });
-                } else if (mediaType === 'video') {
-                    await gss.sendMessage({ video: fileBuffer, mimetype: 'video/mp4', filename: fileName }, { quoted: m });
-                } else {
-                    throw new Error('Unsupported media type');
-                }
-            }
+                    if (isDocument) {
+            await gss.sendMessage(m.chat, { document: fileBuffer, mimetype: `video/mp4`, fileName, caption: 'Downloaded by gss botwa' }, { quoted: m });
         } else {
-            throw new Error('Media URL not found in API response');
+            if (media.type === 'image') {
+                await gss.sendMessage(m.chat, { image: fileBuffer, mimetype: 'image/jpeg', fileName, caption: 'Downloaded by gss botwa' }, { quoted: m });
+            } else if (media.type === 'video') {
+                await gss.sendMessage(m.chat, { video: fileBuffer, mimetype: 'video/mp4', fileName, caption: 'Downloaded by gss botwa' }, { quoted: m });
+            } else {
+                throw new Error('Unsupported media type');
+            }
         }
     } catch (error) {
         console.error('Error while processing Instagram media:', error);
